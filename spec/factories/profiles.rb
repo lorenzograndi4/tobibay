bc4ff22d7f26d22d3d21cdca5a9de8368b194af6
YOUR NAME <muvanlier@gmail.com>
2017-07-11 12:19:39 +0200

    added factories for profile and users

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :profile do
-    first_name "MyString"
-    last_name "MyString"
-    user nil
+    name  { Faker::Name.first_name }
+    address   { Faker::Name.last_name }
+    #bio         { Faker::Lorem.sentence }
   end
 end
