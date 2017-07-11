4ae4f1cb63094233db7ca135b9afbc305d3d8ab2
tob <tobiadonati@gmail.com>
2017-07-11 11:47:32 +0200

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    resources :carts [:new, :create, :show]
+
+
+  devise_for :users, only: [:create]
+
+  resources :users  do
+    resources :carts, only: [:create, :show]
+    resources :profile, only: [:create, :show, :edit]
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products, only: [:index, :show, :destroy]
 end
