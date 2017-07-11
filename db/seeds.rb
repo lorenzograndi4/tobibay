d40fc70d6b3cf13f6dcc19982fce733ec063ef28
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-07-11 12:47:20 +0200

    add images to index and show view only from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,11 @@
 
 Product.destroy_all
 
-product1 = Product.create(name: "chainsaw", price: "199.00", description: "careful")
-product2 = Product.create(name: "umbrella", price: "17.00", description: "use me")
-product3 = Product.create(name: "spoon", price: "3.50", description: "love you")
-product4 = Product.create(name: "corkscrew", price: "5.00", description: "hi!")
+product1 = Product.create(name: "chainsaw", price: "199.00", description: "careful", image: "http://res.cloudinary.com/lorenzocloudinary/image/upload/v1499768608/codaisseurify/chainsaw.jpg")
+product2 = Product.create(name: "umbrella", price: "17.00", description: "use me", image: "http://res.cloudinary.com/lorenzocloudinary/image/upload/v1499768608/codaisseurify/umbrella.jpg")
+product3 = Product.create(name: "spoon", price: "3.50", description: "love you", image: "http://res.cloudinary.com/lorenzocloudinary/image/upload/v1499768608/codaisseurify/spoon.jpg")
+product4 = Product.create(name: "corkscrew", price: "5.00", description: "hi!", image: "http://res.cloudinary.com/lorenzocloudinary/image/upload/v1499768609/codaisseurify/corkscrew.jpg")
+product5 = Product.create(name: "drill", price: "333.50", description: "mysterious", image: "http://res.cloudinary.com/lorenzocloudinary/image/upload/v1499769594/codaisseurify/drill.jpg")
+product6 = Product.create(name: "agenda", price: "15.00", description: "whatami", image: "http://res.cloudinary.com/lorenzocloudinary/image/upload/v1499769698/codaisseurify/agenda.jpg")
 
 puts "#{Product.all.count} products created."
