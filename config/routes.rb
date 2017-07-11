b80b95d413684c41008b1623c48facbfb0aac010
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-07-11 11:33:45 +0200

    add product controller and seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    resources :carts [:new, :create, :show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products, only: [:index, :show, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Generate products
+
+Product.destroy_all
+
+product1 = Product.create(name: "chainsaw", price: "199.00", description: "careful")
+product2 = Product.create(name: "umbrella", price: "17.00", description: "use me")
+product3 = Product.create(name: "spoon", price: "3.50", description: "love you")
+product4 = Product.create(name: "corkscrew", price: "5.00", description: "hi!")
+
+puts "#{Product.all.count} products created."
