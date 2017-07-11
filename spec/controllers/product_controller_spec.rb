337e171aaf5569bbab123d2fbd28601b7ae54d52
tob <tobiadonati@gmail.com>
2017-07-11 14:19:50 +0200

    profile views

--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ProductController, type: :controller do
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:price) }
+  end
 
+  # describe "associations" do
+  #   it { is_expected.to belongs_to(:user) }
+  #   it { is_expected.to have_many(:users) }
+  # end
 end
