a93e1ad771819a7edd706fb7bd3879624e74bdd4
tob <tobiadonati@gmail.com>
2017-07-14 16:39:34 +0200

    release before demo

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -5,42 +5,42 @@ function updateCounters() {
   $("#completed-count").html($(".selected").size());
 }
 
-function createProduct(name) {
-
-  var newProduct = { name: name, price: 111 };
-
-  $.ajax({
-    type: "POST",
-    url: "/products.json",
-    data: JSON.stringify({
-        product: newProduct
-    }),
-    contentType: "application/json",
-    dataType: "json"
-  })
-  .done(function(data) {
-    console.log(data);
-
-    var productId = "product-" + nextProductId();
-
-    var label = $('<label></label>')
-      .attr('for', productId)
-      .html(name);
-    var tableRow = $('<tr class="todo"></td>')
-      .append($('<td>').append(checkbox))
-      .append($('<td>').append(label));
-
-    $("#product-list").append( tableRow );
-
-    updateCounters();
-  }
-
-function submitProduct(event) {
-  event.preventDefault();
-  createProduct($("#product_name").val());
-  $("#product_name").val(null);
-  updateCounters();
-}
+// function createProduct(name) {
+//
+//   var newProduct = { name: name, price: 111 };
+//
+//   $.ajax({
+//     type: "POST",
+//     url: "/products.json",
+//     data: JSON.stringify({
+//         product: newProduct
+//     }),
+//     contentType: "application/json",
+//     dataType: "json"
+//   })
+//   .done(function(data) {
+//     console.log(data);
+//
+//     var productId = "product-" + nextProductId();
+//
+//     var label = $('<label></label>')
+//       .attr('for', productId)
+//       .html(name);
+//     var tableRow = $('<tr class="todo"></td>')
+//       .append($('<td>').append(checkbox))
+//       .append($('<td>').append(label));
+//
+//     $("#product-list").append( tableRow );
+//
+//     updateCounters();
+//   }
+//
+// function submitProduct(event) {
+//   event.preventDefault();
+//   createProduct($("#product_name").val());
+//   $("#product_name").val(null);
+//   updateCounters();
+// }
 
 $(document).ready(function() {
   updateCounters();
